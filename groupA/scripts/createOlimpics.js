16de8a943975e6e4ebb0aa2bdea11e3dd11cad85
teseoch <teseo@Teseo-3.local>
2014-07-18 11:53:14 +0200

    first page finished

--- groupA/scripts/createOlimpics.js
+++ groupA/scripts/createOlimpics.js
@@ -1,5 +1,5 @@
 
-function createCirle(cx,cy,col, radius, clazz)
+function createCirle(cx,cy,col, radius, clazz, dataName,scale)
 {
 	d3.select('#headerSVG')
 	.append("circle")
@@ -9,18 +9,19 @@ function createCirle(cx,cy,col, radius, clazz)
 	.attr('fill','none')
 	.attr('stroke',col)
 	.attr('class',clazz+', ring')
-	.attr('stroke-width',6);
+	.attr('stroke-width',6)
+	.on("click", function(d){ loadTagCloud(dataName,scale); });
 
 }
 
-createCirle(width/2-2*radius-offset,radius+offset,'#0081C6',radius,'europe');
+createCirle(width/2-2*radius-offset,radius+offset,'#0081C6',radius,'europe','Europa',4);
 
-createCirle(width/2+2*radius+offset,radius+offset,'#ED1849',radius,'america');
+createCirle(width/2+2*radius+offset,radius+offset,'#ED1849',radius,'america','America',3);
 
-createCirle(width/2-radius-offset/2,radius+offset+radius,'#FCB034',radius,'asia');
-createCirle(width/2+radius+offset/2,radius+offset+radius,'#00A94F',radius,'ocenania');
+createCirle(width/2-radius-offset/2,radius+offset+radius,'#FCB034',radius,'asia','Asia',3);
+createCirle(width/2+radius+offset/2,radius+offset+radius,'#00A94F',radius,'ocenania','Oceania',2);
 
-createCirle(width/2,radius+offset,'#292526',radius,'africa');
+createCirle(width/2,radius+offset,'#292526',radius,'africa','Africa',0.5);
 
 d3.select('#headerSVG').attr('width',width);
 d3.select('#headerSVG').attr('height',2*offset+3*radius);
